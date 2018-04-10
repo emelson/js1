b2f870f7da1ac347e24319e5391ef2c51549d49f
Eric <ericmelson@gmail.com>
2018-04-09 20:59:56 -0700

    Worked on var and alert for java script.

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var myHistory = "I wanted to get out of my current career field and technology always seemed like the place I wanted to be.  Even within my current job, I always find a way into technology. I researched for a while and then once I moved back to Fresno I found Geekwise.  I took the first two classes and here we are in class 3.";
+//console.log(myHistory);
+var myValue = confirm(myHistory);
+console.log(myValue);
