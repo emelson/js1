9a5db524032c0b0442481b3db727d76daff9fb4c
Eric <ericmelson@gmail.com>
2018-04-11 16:25:08 -0700

    Added text to console log about what I want out of js1 and my favorite hobby.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,2 @@
-var myHistory = "I wanted to get out of my current career field and technology always seemed like the place I wanted to be.  Even within my current job, I always find a way into technology. I researched for a while and then once I moved back to Fresno I found Geekwise.  I took the first two classes and here we are in class 3.";
-//console.log(myHistory);
-var myValue = confirm(myHistory);
-console.log(myValue);
+var myHobby = "I'm taking this class because I really love to use technology in my classes I teach.  I'm hoping to be able to create the best school music program website in the short future, and also a website for the restaurant my wife works at. Ultimately I would love to make a move out of teaching into web development in the longer future. In the small amount of spare time I have, I love to create digital music as well as play the drums.  I'm also interested in Japanese culture because my wife is from Japan.";
+console.log(myHobby);
