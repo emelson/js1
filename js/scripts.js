4ba36cfe6538f319bef675808beac86e159b1584
Eric <ericmelson@gmail.com>
2018-04-12 19:45:06 -0700

    add thank you

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,6 @@ alert('Whats up ' + lastName +'?');
 var userBirthday = prompt('What is your birthday?')
 userBirthday = new Date(userBirthday);
 alert(lastName + '! ' + 'You were born on ' + userBirthday);
+
+const thankYou = 'Thank You!';
+alert(thankYou);
