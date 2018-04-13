8e129678ff141d3a5d2ec75ff73be2c7e050d760
Eric <ericmelson@gmail.com>
2018-04-12 19:47:25 -0700

    added app.js file.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const firstName = prompt('What is your FIRST name?');
+console.log('My name is ' + firstName);
+
+const lastName = prompt('What is your LAST name?');
+alert('Whats up ' + lastName +'?');
+
+var userBirthday = prompt('What is your birthday?')
+userBirthday = new Date(userBirthday);
+alert(lastName + '! ' + 'You were born on ' + userBirthday);
+
+const thankYou = 'Thank You!';
+alert(thankYou);
