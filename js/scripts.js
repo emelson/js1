6867b99c37d8d9187cc102ff288e1b727aed44a7
Eric <ericmelson@gmail.com>
2018-04-12 19:38:27 -0700

    Addded thank you at the end.

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-const firstName = prompt('What is your FIRST name?');
-console.log('My name is ' + firstName);
-
-const lastName = prompt('What is your LAST name?');
-alert('Whats up ' + lastName +'?');
-
-var userBirthday = prompt('What is your birthday?')
-userBirthday = new Date(userBirthday);
-alert(lastName + '! ' + 'You were born on ' + userBirthday);
